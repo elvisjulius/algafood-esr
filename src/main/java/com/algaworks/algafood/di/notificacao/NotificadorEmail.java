b1c19de41976df30d3f1f16b4908377b3a98556b
Elvis <elvisjulius@hotmail.com>
2023-01-22 20:24:43 -0300

    2.13. Usando @Configuration e @Bean para definir beans

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -3,16 +3,29 @@ package com.algaworks.algafood.di.notificacao;
 import com.algaworks.algafood.di.modelo.Cliente;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class NotificadorEmail implements Notificador {
 
-    public NotificadorEmail(){
+    private boolean caixaAlta;
+    private String hostServidorSmtp;
+
+
+    public NotificadorEmail(String hostServidorSmtp){
+        this.hostServidorSmtp = hostServidorSmtp;
         System.out.println("NotificadorEmail");
     }
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        System.out.printf("Notificando %s através do e-mail %s: %s\n",
-                cliente.getNome(),cliente.getEmail(),mensagem);
+
+        if (this.caixaAlta){
+            mensagem = mensagem.toUpperCase();
+        }
+        System.out.printf("Notificando %s através do e-mail %s: %s usando SMTP %s\n",
+                cliente.getNome(),cliente.getEmail(),mensagem, hostServidorSmtp);
+    }
+
+    public void setCaixaAlta(boolean caixaAlta) {
+        this.caixaAlta = caixaAlta;
     }
 }
