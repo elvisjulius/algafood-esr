f33158cb2233aeafb70d7c98b52e4a10d180582e
Elvis <elvisjulius@hotmail.com>
2023-07-01 23:01:20 -0300

    4.34. Finalizando a atualização parcial com a API de Reflections do Spring

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -4,12 +4,15 @@ import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
@@ -77,16 +80,22 @@ public class RestauranteController {
             ResponseEntity.notFound().build();
         }
 
-
         merge(campos, restauranteAtual);
 
         return atualizar(restauranteId, restauranteAtual);
-
     }
 
-    private static void merge(Map<String, Object> campos, Restaurante restauranteDestino) {
-        campos.forEach((nomePropriedade, valorPropriedade) ->{
-        
+    private static void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+
+        dadosOrigem.forEach((nomePropriedade, valorPropriedade) ->{
+            Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+            field.setAccessible(true);
+
+            Object novoValor = ReflectionUtils.getField(field,restauranteOrigem);
+            ReflectionUtils.setField(field,restauranteDestino,novoValor);
         });
     }
 
