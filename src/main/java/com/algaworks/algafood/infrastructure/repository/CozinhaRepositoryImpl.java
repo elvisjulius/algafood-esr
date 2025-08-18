9e8583501566108a71a7665303ec7b18a249b59f
Elvis Julius <elvis.olopes@mercadolivre.com>
2025-08-17 22:35:01 -0300

    5.1. Implementando consultas JPQL em repositórios

--- src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -31,7 +31,7 @@ public class CozinhaRepositoryImpl implements CozinhaRepository {
     @Override
     public List<Cozinha> consultarPorNome(String nome) {
         return manager.createQuery("from Cozinha where nome like :nome", Cozinha.class)
-                .setParameter("nome", nome)
+                .setParameter("nome", "%" +  nome + "%")
                 .getResultList();
     }
 
