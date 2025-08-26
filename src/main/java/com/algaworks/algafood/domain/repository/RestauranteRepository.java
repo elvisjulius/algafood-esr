152f0399812a6bb1364d710496b87e845a08d958
Elvis Julius <elvis.olopes@mercadolivre.com>
2025-08-25 23:21:23 -0300

    5.7. Usando as keywords para definir critérios de query methods

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -5,7 +5,13 @@ import com.algaworks.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
+    List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal);
+
+    List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
 }
